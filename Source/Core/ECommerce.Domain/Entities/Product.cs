24ffae50326b051527424afb31c22fe821900c0b
erknyldrm <erkan-84@hotmail.com>
2022-03-05 17:55:47 +0300

    Infrastructure added.

---
+++ Source/Core/ECommerce.Domain/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿using ECommerce.Domain.Entities.Common;
+
+namespace ECommerce.Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string? Name { get; set; }
+        public int Stock { get; set; }
+        public long Price { get; set; }
+    }
+}
---
+++ Source/Infrastructure/ECommerce.Persistence/ServiceRegistration.cs
@@ -0,0 +1,14 @@
+﻿using ECommerce.Application.Abstractions;
+using ECommerce.Persistence.Concretes;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ECommerce.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceService(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<IProductService, ProductService>();
+        }
+    }
+}
