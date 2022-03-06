8bf9f0ebfe521a7887a6ca2b23eed876212c155a
erknyldrm <erkan-84@hotmail.com>
2022-03-06 15:43:46 +0300

    Db migrated.

---
+++ Source/Core/ECommerce.Domain/Entities/Customer.cs
@@ -0,0 +1,10 @@
+﻿using ECommerce.Domain.Entities.Common;
+
+namespace ECommerce.Domain.Entities
+{
+    public class Customer : BaseEntity
+    {
+        public string Name { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    }
+}
---
+++ Source/Infrastructure/ECommerce.Persistence/Contexts/ECommerceDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ECommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Persistence.Contexts
+{
+    public class ECommerceDbContext : DbContext
+    {
+        public ECommerceDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }    
+    }
+}
